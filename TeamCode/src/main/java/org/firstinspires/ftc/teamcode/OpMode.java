c01d962a34df510210243a9b0af7778475a84909
taylorl <lydia.taylor@saintandrews.net>
2021-04-15 10:32:19 -0400

    ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode.java
@@ -5,79 +5,150 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
 @TeleOp(name = "MecanumSample1", group = "Honors Robotics")
 public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
-        String name;
-        String group;
-        TouchSensor touch;
-        ColorSensor colorSensor;
-        NormalizedColorSensor normalizedColorSensor;
-        RevColorSensorV3 revColorSensorV3;
+    String name;
+    String group;
+    TouchSensor touch;
+    ColorSensor colorSensor;
+    NormalizedColorSensor normalizedColorSensor;
+    RevColorSensorV3 revColorSensorV3;
 
-        DcMotor back_left;
-        DcMotor back_right;
-        DcMotor arm;
-        Servo armServo;
+    DcMotor back_left;
+    DcMotor back_right;
+    DcMotor arm;
+    Servo armServo;
 
 
-        public void armUp(){
-            arm.setPower(1);
-        }
+    public void armUp() {
+        arm.setPower(1);
+    }
 
-        public void armDown(){
-            arm.setPower(-1);
-        }
+    public void armDown() {
+        arm.setPower(-1);
+    }
 
-        float x = gamepad1.left_stick_x;
-        float y = gamepad1.left_stick_y;
-        float xr = gamepad1.right_stick_x;
-        float yr = gamepad1.right_stick_y;
+    float x = gamepad1.left_stick_x;
+    float y = gamepad1.left_stick_y;
+    float xr = gamepad1.right_stick_x;
+    float yr = gamepad1.right_stick_y;
 
-        public void move(){
-            back_right.setPower(y);
-            back_left.setPower(y);
-            telemetry.update();
-            telemetry.addData("The value of x is", x);
-            telemetry.addData("The value of y is",y);
-        }
+    public void move() {
+        back_right.setPower(y);
+        back_left.setPower(y);
+        telemetry.update();
+        telemetry.addData("The value of x is", x);
+        telemetry.addData("The value of y is", y);
+    }
 
-        public void servo(){
-            armServo.setPosition(xr);
-        }
+    public void servo() {
+        armServo.setPosition(xr);
+    }
 
-        public void rotate(){
-            back_right.setPower(-x);
-            back_left.setPower(x);
-            telemetry.update();
-            telemetry.addData("The value of x is", x);
-            telemetry.addData("The value of y is",y);
-        }
+    public void rotate() {
+        back_right.setPower(-x);
+        back_left.setPower(x);
+        telemetry.update();
+        telemetry.addData("The value of x is", x);
+        telemetry.addData("The value of y is", y);
+    }
 
-        public void stop(){
-            if (gamepad1.start) {
-                back_left.setPower(0);
-                back_right.setPower(0);
-                telemetry.update();
-                telemetry.addData("stop", "Stopping");
-            }
-        }
-
-        public void shutDown(){
+    public void stop() {
+        if (gamepad1.start) {
             back_left.setPower(0);
             back_right.setPower(0);
-            arm.setPower(0);
+            telemetry.update();
+            telemetry.addData("stop", "Stopping");
         }
-        public void checkControllerCalibration(){
-            telemetry.addData("Value of left stick y", y);
-            telemetry.addData("Value of left stick x", x);
-            if(y == 0|| x == 0) {
-                if (y == 0) {
-                    telemetry.addData("Y equals 0", y);
-                }
+    }
 
-                if (x == 0) {
-                    telemetry.addData("X equals 0", x);
+    public void shutDown() {
+        back_left.setPower(0);
+        back_right.setPower(0);
+        arm.setPower(0);
+    }
+
+    public void checkControllerCalibration() {
+        telemetry.addData("Value of left stick y", y);
+        telemetry.addData("Value of left stick x", x);
+        if (y == 0 || x == 0) {
+            if (y == 0) {
+                telemetry.addData("Y equals 0", y);
+            }
+
+            if (x == 0) {
+                telemetry.addData("X equals 0", x);
+            }
+        }
+    }
+
+    int timesButtonPressed;
+
+    int DII = 36;
+    double armUp = 7;
+    double armDown = 0;
+    double order = 0;
+    int seconds = timesButtonPressed;
+
+    @Override
+    public void init_loop() {
+
+        if (touch.isPressed() == true) {
+            timesButtonPressed++;
+        }
+    }
+
+        public void autonomous() throws InterruptedException {
+            for (int i = 0; i < timesButtonPressed; i++) {
+                while (arm.getCurrentPosition() < armUp) {
+                    arm.setPower(1);
+                    if (arm.getCurrentPosition() == armUp) {
+                        arm.setPower(0);
+                        arm.setPower(-1);
+                        if (arm.getCurrentPosition() == armDown) {
+                            arm.setPower(0);
+                            order++;
+                        }
+                    }
+                }
+            }
+            while (order == 1) {
+                Thread.sleep(timesButtonPressed);
+                order++;
+            }
+
+            while (order == 2) {
+                back_left.setPower(0);
+                back_right.setPower(0);
+
+                back_left.getMotorType().getTicksPerRev();
+                back_right.getMotorType().getTicksPerRev();
+
+                back_left.getCurrentPosition();
+                back_left.getCurrentPosition();
+
+
+                double e = 72 * DII;
+                while (back_left.getCurrentPosition() < e) {
+                    if (colorSensor.red() > colorSensor.blue() && colorSensor.red() > colorSensor.green()) {
+                        back_left.setPower(0);
+                        back_right.setPower(0);
+                    }
+                    if (colorSensor.blue() > colorSensor.red() && colorSensor.blue()>colorSensor.green()) {
+                        back_left.setPower(0.5);
+                        back_right.setPower(0.5);
+                    }
+                    if (colorSensor.green() > colorSensor.red() && colorSensor.green() > colorSensor.blue()) {
+                        back_left.setPower(1);
+                        back_right.setPower(1);
+                    }
+                    if (back_left.getCurrentPosition() == x) {
+                        back_right.setPower(0);
+                        back_left.setPower(0);
+                    }
                 }
             }
         }
+
+
 
 
         @Override
@@ -88,6 +159,7 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode {
             revColorSensorV3 = (RevColorSensorV3) hardwareMap.colorSensor.get("colorSensor");
 
             touch = hardwareMap.touchSensor.get("touchSensor");
+
         }
 
         @Override
